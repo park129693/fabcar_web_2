dd369a80863f349e81114c871aa230efe9b28b16
park young jun <park129693@gmail.com>
2020-07-08 11:17:28 +0900

    app.js router.js views 작성

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
 var express = require('express')
-var app = express
+var path = require('path')
+var bodyParser = require('body-parser')
+var app = express()
+var ejs = require('ejs')
+require('dotenv').config()
+var apiRouter = require('./routes/Router')
+
+app.set('views', path.resolve(__dirname + '/views'))
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+var cors = require('cors')
+app.use(cors())
+
+app.use('/', apiRouter)
+
+var port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Server is Starting at http:localhost:${port}`)
+})
