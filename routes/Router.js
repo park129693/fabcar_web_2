81503bec7c0a6f5f85c264a0d9363f86ff38417a
park young jun <park129693@gmail.com>
2020-07-08 12:06:41 +0900

    index data table 추가

--- routes/Router.js
+++ routes/Router.js
@@ -1,7 +1,10 @@
 var router = require('express').Router()
+var queryUtil = require('../utils/Util')
 
-router.get('/',(req, res, next)=>{
-    res.render('index')
+router.get('/', async(req, res, next)=>{
+    var result = await queryUtil()
+    var resultData =  await JSON.parse(result)
+    console.log(resultData)
+    res.render('index',{data:resultData})
 })
-
 module.exports = router;
